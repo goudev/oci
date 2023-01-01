d3242cc3cfc8a89e5d38c81a1057b3d03d9b6a04
Junior <gouveiaeu@gmail.com>
2023-01-01 14:51:40 -0300

    ajustes

--- src/regions.js
+++ src/regions.js
@@ -89,47 +89,6 @@ class Regions {
              }
         })
     }
-
-    /**
-     * Obtem a lista de todos os boot-volumes
-     */
-     getAllBootVolumes(){
-        
-        /**
-         * Retorna a promise
-         */
-        return new Promise(async(resolve,reject)=>{
-
-            /**
-             * Define um array para armazenar
-             */
-            var bootVolumes = [];
-
-            /**
-             * Consulta a lista de bootVolumes
-             */
-            new resourceSearch(this.#provider,"allRegions").find("bootvolume resources where (lifecycleState = 'AVAILABLE')").then(async bvs=>{
-
-                /**
-                 * Varre a lista de boot volumes
-                 */
-                for (const bv of bvs) {
-                    await this.getBootVolume(bv.identifier).then(b=>{
-                        bootVolumes.push(b);
-                    }).catch(error=>{
-                        reject("Erro ao consultar o disco " + bv.identifier + "\n\n" + error.message || error)
-                    })
-                }
-
-                /**
-                 * Retorna
-                 */
-                resolve(bootVolumes)
-            }).catch(error=>{
-                reject(error);
-            })
-        })
-    }
 }
 
 module.exports = Regions
