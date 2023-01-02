1ea2465f054dd5c48000bebb9b6d69fda84088ea
Junior <gouveiaeu@gmail.com>
2023-01-02 17:57:45 -0300

    melhorias

--- src/network.js
+++ src/network.js
@@ -140,7 +140,7 @@ class Network {
             /**
              * Trace
              */
-            trace(`Consultando a lista de vnics da tenancy ${this.#provider.delegate.tenancy} na região ${this.#provider.getRegion().regionName}`);
+            trace(`Consultando a lista de vnics da tenancy ${this.#provider.delegate.tenancy} na região ${this.#provider.getRegion()._regionId}`);
 
             /**
              * Consulta a lista de bootVolumes
