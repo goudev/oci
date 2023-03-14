1593912cb95e2f17193180b9ccee98e43569c977
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-03-14 12:14:19 -0300

    feat: listSummarizedUsage functionality

--- src/oci.js
+++ src/oci.js
@@ -5,6 +5,7 @@ var BootVolumes = require("./bootVolumes")
 var BlockVolumes = require("./blockVolumes")
 var ResourceSearch = require("./resourceSearch")
 var Compartments = require("./compartments")
+var Usage = require("./usage")
 var Regions = require("./regions")
 var Network = require("./network")
 var Compute = require("./compute")
@@ -12,7 +13,7 @@ var Region = require("./regions")
 var Monitoring = require("./monitoring")
 
 module.exports = class oci {
-    
+
     #console = console;
     #configFile = "";
     #user = "";
@@ -32,9 +33,9 @@ module.exports = class oci {
         this.#fingerprint = config.fingerprint
         this.#tenancy = config.tenancy
         this.#region = config.region
-        this.#key =  config.private_key
+        this.#key = config.private_key
         this.#inst = hash(JSON.stringify(config));
-        
+
         /**
          * Arquivo config
          */
@@ -50,16 +51,16 @@ module.exports = class oci {
          */
         var wrcfg = fs.writeFile(`/tmp/oci-${this.#inst}.oci`, this.#configFile);
 
-         /**
-          * Grava o arquivo key
-          */
+        /**
+         * Grava o arquivo key
+         */
         var wrkey = fs.writeFile(`/tmp/oci-${this.#inst}.pem`, this.#key);
 
         /**
          * retorna promise
          */
-        return new Promise((resolve,reject)=>{
-            Promise.all([wrcfg,wrkey]).then(async()=>{
+        return new Promise((resolve, reject) => {
+            Promise.all([wrcfg, wrkey]).then(async () => {
 
                 /**
                  * Define o arquivo de configuração
@@ -73,26 +74,26 @@ module.exports = class oci {
 
             }).catch(reject);
         })
-        
+
     }
 
-    #disableConsole(){
+    #disableConsole() {
         console = {
-            log: function(){},
-            error: function(){},
-            info: function(){},
-            warn: function(){}
+            log: function () { },
+            error: function () { },
+            info: function () { },
+            warn: function () { }
         }
     }
 
-    #enableConsole(){
+    #enableConsole() {
         console = this.#console;
     }
 
     /**
      * Seta a região
      */
-    setRegion(region){
+    setRegion(region) {
         try {
 
             /**
@@ -116,174 +117,183 @@ module.exports = class oci {
             return this;
         } catch (error) {
 
-             /**
-              * Habilita o console
-              */
+            /**
+             * Habilita o console
+             */
             this.#enableConsole();
 
             /**
              * Retorna o erro
              */
             return new Error("Erro ao setar a região. \n\n" + error.message || error);
-        }        
+        }
     }
 
-    searchResource(queryString, AllRegions){
-        return new ResourceSearch(this.#provider).find(queryString,AllRegions)
+    searchResource(queryString, AllRegions) {
+        return new ResourceSearch(this.#provider).find(queryString, AllRegions)
     }
 
-    getBlockVolume(volumeId){
+    getBlockVolume(volumeId) {
         return new BlockVolumes(this.#provider).getBlockVolume(volumeId);
     }
 
-    listBlockVolumes(){
+    listBlockVolumes() {
         return new BlockVolumes(this.#provider).listBlockVolumes();
     }
 
-    getBootVolume(volumeId){
+    getBootVolume(volumeId) {
         return new BootVolumes(this.#provider).getBootVolume(volumeId);
     }
 
-    listBootVolumes(){
+    listBootVolumes() {
         return new BootVolumes(this.#provider).listBootVolumes();
     }
 
-    listBootVolumeAttachments(){
+    listBootVolumeAttachments() {
         return new BootVolumes(this.#provider).listBootVolumeAttachments();
     }
 
-    listBlockVolumeAttachments(){
+    listBlockVolumeAttachments() {
         return new BlockVolumes(this.#provider).listBlockVolumeAttachments();
     }
 
-    getRegionSubscriptions(){
+    getRegionSubscriptions() {
         return new Regions(this.#provider).getRegionSubscriptions();
     }
 
-    listCompartments(){
+    listCompartments() {
         return new Compartments(this.#provider).listCompartments();
     }
 
-    listVnicAttachments(){
+    listSummarizedUsage({ resourceId, startDate, endDate, granularity }) {
+        return new Usage(this.#provider).listSummarizedUsage(
+            resourceId,
+            startDate,
+            endDate,
+            granularity
+        );
+    }
+
+    listVnicAttachments() {
         return new Network(this.#provider).listVnicAttachments();
     }
 
-    listVnics(){
+    listVnics() {
         return new Network(this.#provider).listVnics();
     }
 
-    getVnic(vnicId){
+    getVnic(vnicId) {
         return new Network(this.#provider).getVnic(vnicId);
     }
 
-    listPublicIps(){
+    listPublicIps() {
         return new Network(this.#provider).listPublicIps();
     }
 
-    getPublicIp(publicIpId){
+    getPublicIp(publicIpId) {
         return new Network(this.#provider).getPublicIp(publicIpId);
     }
 
-    listPrivateIps(){
+    listPrivateIps() {
         return new Network(this.#provider).listPrivateIps();
     }
 
-    getPrivateIp(privateIpId){
+    getPrivateIp(privateIpId) {
         return new Network(this.#provider).getPrivateIp(privateIpId);
     }
 
-    listVcns(){
+    listVcns() {
         return new Network(this.#provider).listVcns();
     }
 
-    getVcn(vcnId){
+    getVcn(vcnId) {
         return new Network(this.#provider).getVcn(vcnId);
     }
 
-    listSubnets(){
+    listSubnets() {
         return new Network(this.#provider).listSubnets();
     }
 
-    getSubnet(subnetId){
+    getSubnet(subnetId) {
         return new Network(this.#provider).getSubnet(subnetId);
     }
 
-    listSecurityLists(){
+    listSecurityLists() {
         return new Network(this.#provider).listSecurityLists();
     }
 
-    getSecurityList(securityListId){
+    getSecurityList(securityListId) {
         return new Network(this.#provider).getSecurityList(securityListId);
     }
 
-    listNetworkSecurityGroups(){
+    listNetworkSecurityGroups() {
         return new Network(this.#provider).listNetworkSecurityGroups();
     }
 
-    getNetworkSecurityGroup(networkSecurityGroupId){
+    getNetworkSecurityGroup(networkSecurityGroupId) {
         return new Network(this.#provider).getNetworkSecurityGroup(networkSecurityGroupId);
     }
 
-    listRouteTables(){
+    listRouteTables() {
         return new Network(this.#provider).listRouteTables();
     }
 
-    getRouteTable(routeTableId){
+    getRouteTable(routeTableId) {
         return new Network(this.#provider).getRouteTable(routeTableId);
     }
 
-    listNatGateways(){
+    listNatGateways() {
         return new Network(this.#provider).listNatGateways();
     }
 
-    getNatGateway(natGatewayId){
+    getNatGateway(natGatewayId) {
         return new Network(this.#provider).getNatGateway(natGatewayId);
     }
 
-    listServiceGateways(){
+    listServiceGateways() {
         return new Network(this.#provider).listServiceGateways();
     }
 
-    getServiceGateway(serviceGatewayId){
+    getServiceGateway(serviceGatewayId) {
         return new Network(this.#provider).getServiceGateway(serviceGatewayId);
     }
 
-    listImages(){
+    listImages() {
         return new Compute(this.#provider).listImages();
     }
 
-    getImage(imageId){
+    getImage(imageId) {
         return new Compute(this.#provider).getImage(imageId);
     }
 
-    listInstances(){
+    listInstances() {
         return new Compute(this.#provider).listInstances();
     }
 
-    getInstance(instanceId){
+    getInstance(instanceId) {
         return new Compute(this.#provider).getInstance(instanceId);
     }
 
-    listRegionSubscriptions(){
+    listRegionSubscriptions() {
         return new Region(this.#provider).listRegionSubscriptions();
     }
 
-    getCpuMetrics(resourceId,days,interval){
-        return new Monitoring(this.#provider).getCpuMetrics(resourceId,days,interval);
+    getCpuMetrics(resourceId, days, interval) {
+        return new Monitoring(this.#provider).getCpuMetrics(resourceId, days, interval);
     }
 
-    getMemoryMetrics(instanceData,days,interval){
-        return new Monitoring(this.#provider).getMemoryMetrics(instanceData,days,interval);
-    }
-    
-
-    getCpuUsage(resourceId,days,interval){
-        return new Monitoring(this.#provider).getCpuUsage(resourceId,days,interval);
+    getMemoryMetrics(instanceData, days, interval) {
+        return new Monitoring(this.#provider).getMemoryMetrics(instanceData, days, interval);
     }
 
-    getMemoryUsage(resourceId,days,interval){
-        return new Monitoring(this.#provider).getMemoryUsage(resourceId,days,interval);
+
+    getCpuUsage(resourceId, days, interval) {
+        return new Monitoring(this.#provider).getCpuUsage(resourceId, days, interval);
     }
-    
-    
+
+    getMemoryUsage(resourceId, days, interval) {
+        return new Monitoring(this.#provider).getMemoryUsage(resourceId, days, interval);
+    }
+
+
 }
