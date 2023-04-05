852a97f55366d2c54b3b190a7ffdf8369ff02f65
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-04-05 08:58:21 -0300

    feat: listar clusters por cada compartment

--- src/clusters.js
+++ src/clusters.js
@@ -1,40 +1,127 @@
 let Util = require("./util");
 const containerengine = require("oci-containerengine");
+const Compartments = require("./compartments");
+const { setTimeout } = require('timers/promises');
 
 class Cluster {
 
     #provider = "";
     #util = ""
 
-    constructor(provider){
+    constructor(provider) {
         this.#provider = provider;
         this.#util = new Util();
         return this;
     }
 
+    listClusters(compartmentId) {
+
+        /**
+         * Retorna a promise
+         */
+        return new Promise(async (resolve, reject) => {
+
+            /**
+             * Cria um array para armazenar as informações
+             */
+            var cls = [];
+
+            /**
+             * Se foi passado um compartimento
+             */
+            if (compartmentId) {
+
+                /**
+                 * Habilita o console
+                 */
+                this.#util.disableConsole();
+
+                /**
+                 * Realiza a consulta
+                 */
+                new containerengine.ContainerEngineClient({ authenticationDetailsProvider: this.#provider }).listClusters({
+                    compartmentId: compartmentId
+                }).then(result => {
+
+                    /**
+                     * Habilita o console
+                     */
+                    this.#util.enableConsole();
+
+                    /**
+                     * retorna a promise
+                     */
+                    resolve(result.items);
+
+                }).catch(error => {
+                    /**
+                     * Habilita o console
+                     */
+                    this.#util.enableConsole();
+
+                    /**
+                     * Rejeita a promise
+                     */
+                    reject("Erro ao obter a lista de Block Volume Attachments. \n\n" + error.message || error);
+                });
+
+            } else {
+
+                /**
+                 * Obtem a lista de compartimentos
+                 */
+                new Compartments(this.#provider).listCompartments().then(async compartments => {
+                    for (const compartment of compartments) {
+
+                        /**
+                         * Obtem a lista de compartimentos
+                         */
+                        await this.listClusters(compartment.id).then(result => {
+                            result.forEach(c => {
+                                cls.push(c)
+                            });
+                        }).catch(error => {
+                            reject(error.message || error)
+                        })
+                    }
+                    resolve(cls)
+                }).catch(error => {
+
+                    /**
+                     * Rejeita a promise
+                     */
+                    reject(error.message);
+                })
+            }
+        })
+    }
+
     /**
      * Lists all clusters
      */
-    async listClusters(compartmentId) {
-        try {
-            // Creates a client
-            const client = new containerengine.ContainerEngineClient({
-                authenticationDetailsProvider: this.#provider,
-            });
+    async k() {
+        return new Promise((resolve, reject) => {
+            new Compartments(this.#provider).listCompartments().then(async compartments => {
+                const client = new containerengine.ContainerEngineClient({
+                    authenticationDetailsProvider: this.#provider,
+                });
 
-            // Request details
-            const listClustersRequest = {
-                compartmentId,
-                lifecycleState: [containerengine.models.ClusterLifecycleState.Active],
-            };
+                const result = [];
 
-            // Send request to the Client.
-            const listClustersResponse = await client.listClusters(listClustersRequest);
+                for (const compartment of compartments) {
+                    const listClustersRequest = {
+                        compartmentId: compartment.id,
+                        lifecycleState: [containerengine.models.ClusterLifecycleState.Active],
+                    };
 
-            return listClustersResponse.items;
-        } catch (error) {
-            return error;
-        }
+                    const listClustersResponse = await client.listClusters(listClustersRequest);
+                    result.push(...listClustersResponse.items);
+                    await setTimeout(2000);
+                }
+
+                resolve(result);
+            })
+        })
     }
 }
 
